c15bc3e6147e3e950640741d7499198c83d1d6bc
Renan1fps <renan.sergipe68@gmail.com>
2025-12-26 16:53:52 -0300

    feat: add base server with express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('Base compile project');
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(3025, () => console.log(`Server started on port 3025`));
+
